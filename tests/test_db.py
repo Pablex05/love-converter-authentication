cc57e1be5eff87f8dbe2e05a807d15ba755a1c51
Esteban <estebanarivasv@gmail.com>
2021-05-31 13:40:40 -0300

    Upload unit test folders

---
+++ tests/test_db.py
@@ -0,0 +1,14 @@
+import unittest
+
+from main import create_app
+
+
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
