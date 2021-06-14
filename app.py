b102b4273d6ecd0af9bb6da1b76649ab43d7089b
Matias Ruiz <matiasruiz2015@outlook.com>
2021-06-14 15:26:36 -0300

    Instance DB

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = create_app()
 
 # Loading app context
 app.app_context().push()
+from main import db
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(port=os.getenv('PORT'))
--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
@@ -25,6 +26,10 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_object(DevelopmentConfig())
+
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     # Database initialization in Flask app
     db.init_app(app)
